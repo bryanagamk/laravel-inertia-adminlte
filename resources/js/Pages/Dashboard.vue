cb0fbd5eb34ba7a76751db2e38383fb226c404e7
bryanagamk <bryanagamkottama@gmail.com>
2024-07-03 04:52:08 +0700

    feat: implement vue

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>INI DASHBOARD</h1>
+        <h2>Testing</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
